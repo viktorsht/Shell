4745e5d79f4ba62c3e75a1577f45e36ab1af902b
viktorsht <viktorshantus@gmail.com>
2021-10-09 12:17:03 -0300

    programa para imprimir token

---
+++ token.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Criado em 09/10/2021
+# Por Viktor Santos <viktorshantus@gmail.com>
+# Descrição: Um executável que imprime o token que utilizo para meu usuário de GitHub
+# Programa descritivo para fins de aprendizado!
+
+echo "Meu token Atual"
+cd 
+cat # caminho para seu arquivo
+
